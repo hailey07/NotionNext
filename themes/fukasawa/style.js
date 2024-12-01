865e85ea6915f2862972768ac74972e20ba50494
MoeU33 <67306110+hailey07@users.noreply.github.com>
2024-12-01 20:07:02 +0800

    Update style.js

--- themes/fukasawa/style.js
+++ themes/fukasawa/style.js
@@ -53,6 +53,7 @@ const Style = () => {
     .container {
             display: grid;
             grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: repeat(3, 1fr);
             grid-gap: 10px;
             padding: 10px;
         }
