7e8df797fabca78d3a09b47a670380f4c9176540
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-11 22:51:24 +0800

    update

--- themes/fukasawa/style.js
+++ themes/fukasawa/style.js
@@ -9,6 +9,7 @@ const Style = () => {
       // 底色
       body{
           background-color: #eeedee;
+          
       }
       .dark body{
           background-color: black;
@@ -24,7 +25,7 @@ const Style = () => {
       /* 大屏幕（宽度≥1024px）下显示3列 */
       @media (min-width: 1024px) {
           #theme-fukasawa .grid-container {
-          column-count: 3;
+          column-count: 2;
           column-gap: .5rem;
           }
       }
@@ -32,7 +33,7 @@ const Style = () => {
       /* 小屏幕（宽度≥640px）下显示2列 */
       @media (min-width: 640px) and (max-width: 1023px) {
           #theme-fukasawa .grid-container {
-          column-count: 2;
+          column-count: 1;
           column-gap: .5rem;
           }
       }
