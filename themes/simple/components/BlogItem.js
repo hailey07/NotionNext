15a3dc88c43021ad9aaa7bd1e67235b18ef7853d
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 05:53:19 +0800

    Remove author link from BlogItem component
    Removed author link and related span from BlogItem component.

--- themes/simple/components/BlogItem.js
+++ themes/simple/components/BlogItem.js
@@ -51,14 +51,7 @@ export const BlogItem = props => {
           {/* 文章信息 */}
           <header className='mb-5 text-md text-gray-700 dark:text-gray-300 flex-wrap flex leading-6'>
             <div className='space-x-2'>
-              <span>
-                {' '}
-                <a
-                  href={siteConfig('SIMPLE_AUTHOR_LINK', null, CONFIG)}
-                  className='p-1 hover:text-red-400 transition-all duration-200'>
-                  <i className='fa-regular fa-user'></i> {siteConfig('AUTHOR')}
-                </a>
-              </span>
+             
               <span>
                 <SmartLink
                   className='p-1 hover:text-red-400 transition-all duration-200'
