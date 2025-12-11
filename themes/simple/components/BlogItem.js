00f41f838ca5e090d4b7e2419c10d0d0319c4430
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 07:19:51 +0800

    Fix JSX syntax and formatting issues in BlogItem.js

--- themes/simple/components/BlogItem.js
+++ themes/simple/components/BlogItem.js
@@ -51,7 +51,9 @@ export const BlogItem = props => {
           {/* 文章信息 */}
           <header className='mb-5 text-md text-gray-700 dark:text-gray-300 flex-wrap flex leading-6'>
             <div className='space-x-2'>
-             
+             <span>
+
+              </span>
               <span>
                 <SmartLink
                   className='p-1 hover:text-red-400 transition-all duration-200'
@@ -66,8 +68,8 @@ export const BlogItem = props => {
             </div>
 
             <div>
-              {post。category && (
-                <SmartLink href={`/category/${post.category}`} className='p-1'>
+              {post.category && (
+                <SmartLink href={`/category/${post。category}`} className='p-1'>
                   {' '}
                   <span className='hover:text-red-400 transition-all duration-200'>
                     <i className='fa-regular fa-folder mr-0.5' />
