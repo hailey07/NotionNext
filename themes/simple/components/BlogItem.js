04148b9729651dab76050b41f8637c5e18a6535e
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 07:17:30 +0800

    Fix syntax errors in BlogItem.js

--- themes/simple/components/BlogItem.js
+++ themes/simple/components/BlogItem.js
@@ -51,10 +51,11 @@ export const BlogItem = props => {
           {/* 文章信息 */}
           <header className='mb-5 text-md text-gray-700 dark:text-gray-300 flex-wrap flex leading-6'>
             <div className='space-x-2'>
+             
               <span>
                 <SmartLink
                   className='p-1 hover:text-red-400 transition-all duration-200'
-                  href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}>
+                  href={`/archive#${formatDateFmt(post?.publishDate， 'yyyy-MM')}`}>
                   <i className='fa-regular fa-clock' />{' '}
                   {post.date?.start_date || post.createdTime}
                 </SmartLink>
@@ -65,7 +66,7 @@ export const BlogItem = props => {
             </div>
 
             <div>
-              {post.category && (
+              {post。category && (
                 <SmartLink href={`/category/${post.category}`} className='p-1'>
                   {' '}
                   <span className='hover:text-red-400 transition-all duration-200'>
@@ -74,7 +75,7 @@ export const BlogItem = props => {
                   </span>
                 </SmartLink>
               )}
-              {post?.标签 &&
+              {post?.tags &&
                 post?.tags?.length > 0 &&
                 post?.tags.map(t => (
                   <SmartLink
@@ -91,7 +92,7 @@ export const BlogItem = props => {
             {!showPreview && (
               <>
                 {post.summary}
-                {post.summary && <span>...</span>}
+                {<span>V </span> && post.summary}
               </>
             )}
             {showPreview && post?.blockMap && (
