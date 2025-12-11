1010da866d35f2fe4cfebb7303f3630b66a5ecbf
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 05:31:51 +0800

    Add Gumroad social button to SocialButton

--- themes/simple/components/SocialButton.js
+++ themes/simple/components/SocialButton.js
@@ -108,6 +108,15 @@ const SocialButton = () => {
             <i className='fab fa-threads transform hover:scale-125 duration-150' />
           </a>
         )}
+        {siteConfig('CONTACT_GUMROAD') && (
+          <a
+            target='_blank'
+            rel='noreferrer'
+            title={'threads'}
+            href={siteConfig('CONTACT_GUMROAD')}>
+            <i className='fab fa-gofore transform hover:scale-125 duration-150' />
+          </a>
+         )}
       </div>
     </div>
   )
