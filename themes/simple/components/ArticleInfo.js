408cb8415499385499b9b2d64a29bf6355cc6759
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 08:22:30 +0800

    Update ArticleInfo.js

--- themes/simple/components/ArticleInfo.js
+++ themes/simple/components/ArticleInfo.js
@@ -26,7 +26,7 @@ export default function ArticleInfo (props) {
                 {post?.type !== 'Page' && (
                     <div className="space-x-3 mr-4">
                        
-                        <span> <i className="fa-regular fa-clock"></i> {post?.publishDay}</span>
+                        
                         {post?.category && <span>  <i className="fa-regular fa-folder"></i> <a href={`/category/${post?.category}`} className="hover:text-red-400 transition-all duration-200">{post?.category}</a></span>}
                         {post?.tags && post?.tags?.length > 0 && post?.tags.map(t => <span key={t}> / <SmartLink href={`/tag/${t}`}><span className=' hover:text-red-400 transition-all duration-200'>{t}</span></SmartLink></span>)}
                     </div>)}
