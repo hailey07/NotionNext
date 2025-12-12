75cbf549a7a2d4a3416458c0dbce73a783215170
MoeU33 <67306110+hailey07@users.noreply.github.com>
2025-12-12 11:30:22 +0800

    Update ArticleInfo.js

--- themes/simple/components/ArticleInfo.js
+++ themes/simple/components/ArticleInfo.js
@@ -33,6 +33,7 @@ export default function ArticleInfo (props) {
                 {post?.type !== 'Page' && (<div className=''>
                     <span className='mr-2'>|</span>
                     <span> <i className="fa-regular fa-clock"></i> {post?.publishDay}</span>
+                    <span>  </span>
                     <span className='mr-2'>|</span>
                     <span className='mx-2  dark:text-gray-500'>
                         {locale.COMMON.LAST_EDITED_TIME}: {post?.lastEditedDay}
